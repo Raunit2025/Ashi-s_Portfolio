1ca229e7b28fc5867816a8200d41bb9802d966b7
Raunit2025 <raunitraj2336@gmail.com>
2025-09-07 18:51:35 +0530

    Error fixed

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,9 +1,11 @@
-
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      {/* Test with simple, solid white text */}
+      <h1 className="text-5xl font-bold text-white">
+        Does this text appear?
+      </h1>
+    </div>
   )
 }
 
